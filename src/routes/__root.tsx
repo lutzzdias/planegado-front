7042763fc003bd73802e4aa4ae3756ace401c6cf
lutzzdias <thiagolutzdias@hotmail.com>
2025-09-09 00:42:09 -0400

    chore: initialize project

---
+++ src/routes/__root.tsx
@@ -0,0 +1,43 @@
+/// <reference types="vite/client" />
+import type { ReactNode } from "react";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+import css from "../app.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { title: "Planegado" },
+    ],
+    links: [{ rel: "stylesheet", href: css }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html className="h-full">
+      <head>
+        <HeadContent />
+      </head>
+      <body className="flex h-full flex-col">
+        <main className="flex-1">{children}</main>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
