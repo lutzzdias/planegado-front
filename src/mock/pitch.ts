bcd7967dd32b33d54ad5b08f66f749c5a6dcbfbb
lutzzdias <thiagolutzdias@hotmail.com>
2025-09-17 19:07:09 -0400

    chore: create mock pitch object

---
+++ src/mock/pitch.ts
@@ -0,0 +1,30 @@
+export const PITCH = {
+  initial: {
+    averageWeight: 220,
+    totalWeight: 2200,
+    averageValue: 2342.12,
+    totalValue: 23421.19,
+  },
+  ration: {
+    averageDailyConsumption: 4.31,
+    totalQuantity: 705,
+    totalValue: 7325.16,
+    consumption: [12, 15, 17, 23, 26, 30, 32, 38, 47, 53, 60, 73, 82, 96],
+  },
+  final: {
+    averageWeight: 400,
+    totalWeight: 4000,
+    exitDate: Date.now(),
+    profit: {
+      total: 32157.32,
+      investor: {
+        value: 16078.66,
+        percentage: 50,
+      },
+      producer: {
+        value: 16078.66,
+        percentage: 50,
+      },
+    },
+  },
+};
